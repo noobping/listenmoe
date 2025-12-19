801d379d7b2db04fa529ee8b7de42fe73ca1a2c0
nick <noobping@users.noreply.github.com>
2025-12-19 12:52:11 +0100

    only window app for release

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
 mod http_source;
 mod listen;
