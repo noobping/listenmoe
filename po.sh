38276cd904fedc3acc26eda1ce067e45fb04b6a6
nick <noobping@users.noreply.github.com>
2025-12-21 15:05:32 +0100

    add locales to msi

--- po.sh
+++ po.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 APP_ID=io.github.noobping.listenmoe
+OUTDIR="${1:-data/locale}"
 
 for f in po/*.po; do
     lang=$(basename "$f" .po)
-    mkdir -p "data/locale/$lang/LC_MESSAGES"
-    msgfmt "$f" -o "data/locale/$lang/LC_MESSAGES/$APP_ID.mo"
-    msgfmt "$f" -o "data/locale/$lang/LC_MESSAGES/$APP_ID_develop.mo"
-done
+    mkdir -p "$OUTDIR/$lang/LC_MESSAGES"
+    msgfmt "$f" -o "$OUTDIR/$lang/LC_MESSAGES/$APP_ID.mo"
+    msgfmt "$f" -o "$OUTDIR/$lang/LC_MESSAGES/$APP_ID_develop.mo"
 
-for f in po/*.po; do lang=$(basename "$f" .po)
     mkdir -p "AppDir/share/locale/$lang/LC_MESSAGES"
     msgfmt "$f" -o "AppDir/share/locale/$lang/LC_MESSAGES/$APP_ID.mo"
 done
